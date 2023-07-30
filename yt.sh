ef6bc73187a5b08715012bce96e085aabd6fc822
Jumpi61 <140933850+Jumpi61@users.noreply.github.com>
2023-07-30 00:43:19 -0400

    Update yt.sh

--- yt.sh
+++ yt.sh
@@ -1,1 +1,2 @@
-yt-dlp $1 -f "best" --audio-format $3 -x -o $2 --max-filesize 100m --no-playlist
+filename=$(basename "$2" .tmp)
+yt-dlp "$1" -f "best" --audio-format "$3" -x -o "$filename" --max-filesize 100m --no-playlist
